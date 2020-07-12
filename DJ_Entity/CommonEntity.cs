27ad5f8468dc7e3d79590d8e4d3f309ece6eab15
Prashant <prashant0302k4@gmail.com>
2020-07-13 00:13:19 +0530

    code files 3

---
+++ DJ_Entity/CommonEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJ_Entity
+{
+    public class JsonReurnData
+    {
+        public int ReturnStatus { get; set; }
+        public string ReturnMessage { get; set; }
+        public string ReturnData { get; set; }
+        public string ReturnData2 { get; set; }
+    }
+}
---
+++ DJ_Entity/ExamModels.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DJ_Entity
+{
+    public class ApplicantExamVM
+    {
+        public ApplicantExamAttempt ApplicantAttempt { get; set; }
+        public List<ApplicantAnswer> ApplicantAnswers { get; set; }
+    }
+
+    
+
+    public class QuestionPaper
+    {
+        [Key]
+        public int QuestionPaperID { get; set; }
+        public DJ_Entity.Jobs AppliedJob { get; set; }
+        public DateTime? CreateDateTime { get; set; }
+        public string CreatedBy { get; set; }
+        public string UploadedFilePath { get; set; }
+
+        public List<QuestionSet> Questions { get; set; }
+    }
+
+    public class QuestionSet
+    {
+        public int QNO { get; set; }
+        [Key]
+        public long QuestionID { get; set; }
+        public string Question { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string E { get; set; }
+        public string CorrectAnswer { get; set; }
+        public double MarksForCorrect { get; set; }
+        public double MarksForInCorrect { get; set; }
+        public string AttachmentUrl { get; set; }
+        public string SetNumber { get; set; }
+        public int QuestionPaperID { get; set; }
+        public virtual QuestionPaper QuestionPaper { get; set; }
+
+    }
+
+    public class ApplicantExamAttempt
+    {
+        [Key]
+        public Int64 AttemptID { get; set; }
+        public Int64? ApplicantID { get; set; }
+        public int? QuestionPaperID { get; set; }
+        public virtual DJ_Entity.Applicant Applicant { get; set; }
+        public virtual QuestionPaper QuestionPaper { get; set; }
+        public DateTime? ExamStartDateTime { get; set; }
+        public DateTime? ExamEndDateTime { get; set; }
+        public double? MarksObtained { get; set; }
+        public string EvaluatedBy { get; set; }
+        public string IPAddress { get; set; }
+    }
+
+    public class ApplicantAnswer
+    {
+        [Key]
+        public Int64 AnswerSheetID { get; set; }
+        public Int64? AttemptID { get; set; }
+        public Int64? QuestionID { get; set; }
+        public virtual QuestionSet Question { get; set; }
+        public string AnswerByApplicant { get; set; }
+        public DateTime? CreateDateTime { get; set; }
+        public DateTime? UpdateDateTime { get; set; }
+    }
+
+    /*
+
+
+
+
+      
+     */
+}
